c9a21e11edbab4588ebd2f684272320214751647
Anastasya <anasty22@gmail.com>
2023-01-05 18:26:23 +0200

    add

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { StreamChat } from "stream-chat";
+import Cookies from "universal-cookie";
+import { useState } from "react";
+
+function App() {
+  const api_key = "duh9vykpu5fj";
+  const cookies = new Cookies();
+
+  const client = StreamChat.getInstance(api_key);
+  const [isAuth, setIsAuth] = useState(false);
+  const token = cookies.get("token");
+
+const logOut=()=>{
+  cookies.remove("token");
+  cookies.remove("userId");
+  cookies.remove("firstName");
+  cookies.remove("lastName");
+  cookies.remove("hashedPassword")
+  cookies.remove("channelName");
+  cookies.remove("username");
+  client.disconnectUser()
+  setIsAuth(false)
+
+}
+
+  if (token) {
+    client
+      .connectUser(
+        {
+          id: cookies.get("userId"),
+          name: cookies.get("username"),
+          firstName: cookies.get("firstName"),
+          lastName: cookies.get("lastName"),
+          hashedPassword: cookies.get("hashedPassword"),
+        },
+        token
+      )
+      .then((user) => {
+        setIsAuth(true);
+      });
+  }
+  return (
+    <div className="App">
+      {isAuth ? (
+        <button onClick={logOut}>Log Out</button>
+      ) : (
+        <>
+          <SignUp setIsAuth={setIsAuth} />
+          <Login setIsAuth={setIsAuth} />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
