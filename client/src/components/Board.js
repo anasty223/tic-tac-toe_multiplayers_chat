fe5c81523dc15799508fb3cab32fb6a0bdf30852
Anastasya <anasty22@gmail.com>
2023-01-10 18:31:06 +0200

    upd board

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,29 +1,87 @@
-import React, { useState } from 'react'
-import Square from './Square'
+import React, { useState } from "react";
+import Square from "./Square";
+import {useChannelStateContext,useChatContext} from "stream-chat-react"
 
 function Board() {
-const [board, setBoard] = useState(["X","O","","X","","O","","","",])
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X");
+  const [turn, setTurn] = useState("X");
 
+  const {channel} =useChannelStateContext()
+  const {client}=useChatContext()
+  const chooseSquare = async (square) => {
+    if(turn===player&&board[square]===""){
+        setTurn(player==="X"?  "O":"X")
 
+await channel.sendEvent({
+    type:"game-move",
+    data:{square,player},
+});
+
+        setBoard(
+            board.map((val, idx) => {
+              if (idx === square && val === "") {
+                return player;
+              }
+              return val;
+            })
+          );
+        }
+      };
+channel.on((event)=>{
+    if(event.type==="game-move"&&event.user.id!==client.userID){
+      const currentPlayer=event.data.player==="X" ? "O" : "X"
+      setPlayer(currentPlayer);
+setTurn(currentPlayer);
+
+        setBoard( board.map((val, idx) => {
+          if (idx === event.data.square && val === "") {
+            return event.data.player;
+          }
+          return val;
+        }))
+    }
+})
   return (
     <div className="board">
-        <div className="row">
-            <Square  val={board[0]}/>
-            <Square val={board[1]}/>
-            <Square val={board[2]}/>
-        </div>
-        <div className='row'>
-            <Square val={board[3]}/>
-            <Square val={board[4]}/>
-            <Square val={board[5]}/>
-        </div>
-        <div className='row'>
-            <Square val={board[6]}/>
-            <Square val={board[7]}/>
-            <Square val={board[8]}/>
-        </div>
-        </div>
-  )
+      <div className="row">
+        <Square
+          chooseSquare={() => {
+            chooseSquare(0);
+          }}
+          val={board[0]}
+        />
+        <Square  chooseSquare={() => {
+            chooseSquare(1);
+          }} val={board[1]} />
+        <Square  chooseSquare={() => {
+            chooseSquare(2);
+          }} val={board[2]} />
+      </div>
+      <div className="row">
+        <Square  chooseSquare={() => {
+            chooseSquare(3);
+          }} val={board[3]} />
+        <Square  chooseSquare={() => {
+            chooseSquare(4);
+          }} val={board[4]} />
+        <Square  chooseSquare={() => {
+            chooseSquare(5);
+          }} val={board[5]} />
+      </div>
+      <div className="row">
+        <Square  chooseSquare={() => {
+            chooseSquare(6);
+          }} val={board[6]} />
+        <Square  chooseSquare={() => {
+            chooseSquare(7);
+          }} val={board[7]} />
+        <Square  chooseSquare={() => {
+            chooseSquare(8);
+          }} val={board[8]} />
+      </div>
+    </div>
+  );
 }
 
-export default Board
+export default Board;
